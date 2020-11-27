77868d781bd6415dfb280ccfacaeadf4865ed4f3
QueCue <mfkditruemc@163.com>
2020-11-27 18:54:40 +0800

    文本处理：粗体、斜体、下划线、删除线

--- Memo/Memo/MemoWindow_TextStyle.cs
+++ Memo/Memo/MemoWindow_TextStyle.cs
@@ -6,71 +6,70 @@ namespace Memo
 {
     public partial class MemoWindow
     {
-        private void BoldBtn_Click(object sender, RoutedEventArgs e)
+        private TextSelection Selection => m_mainInput.Selection;
+
+        private TextRange GetFirstSelection()
         {
-            m_mainInput.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+            TextPointer endPos = Selection.Start.GetPositionAtOffset(1);
+            return null == endPos ? null : new TextRange(Selection.Start, endPos);
         }
 
-        private void ItalicBtn_Click(object sender, RoutedEventArgs e)
+        private void HandleText(DependencyProperty formattingProperty, object targetValue, object defaultValue)
         {
-            TextSelection selection = m_mainInput.Selection;
-            TextPointer endPos = selection.Start.GetPositionAtOffset(1);
-            if (null == endPos)
+            TextRange firstSelection = GetFirstSelection();
+            if (null == firstSelection)
             {
                 return;
             }
 
-            m_mainInput.Selection.Select(selection.Start, endPos);
-            //m_mainInput.Focus();
-            //var obj = .GetPropertyValue(TextElement.FontStyleProperty);
-            //if (null == obj
-            //    || (obj is FontStyle fontStyle && fontStyle != FontStyles.Italic))
-            //{
-            //    selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic);
-            //}
-            //else
-            //{
-            //    selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Normal);
-            //}
-            //m_mainInput.Focus();
+            object obj = firstSelection.GetPropertyValue(formattingProperty);
+            if (null == obj
+                || obj.GetType() == targetValue.GetType()
+                && !obj.Equals(targetValue))
+            {
+                Selection.ApplyPropertyValue(formattingProperty, targetValue);
+            }
+            else
+            {
+                Selection.ApplyPropertyValue(formattingProperty, defaultValue);
+            }
+        }
+
+        private void BoldBtn_Click(object sender, RoutedEventArgs e)
+        {
+            HandleText(TextElement.FontWeightProperty, FontWeights.Bold, FontWeights.Normal);
+        }
+
+        private void ItalicBtn_Click(object sender, RoutedEventArgs e)
+        {
+            HandleText(TextElement.FontStyleProperty, FontStyles.Italic, FontStyles.Normal);
         }
 
         private void UnderlineBtn_Click(object sender, RoutedEventArgs e)
         {
-            TextSelection selection = m_mainInput.Selection;
-            if (!selection.IsEmpty)
-            {
-                var tdc = (TextDecorationCollection) selection.GetPropertyValue(Inline.TextDecorationsProperty);
-                if (tdc == null || !tdc.SequenceEqual(TextDecorations.Underline))
-                {
-                    selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
-                }
-                else
-                {
-                    selection.ApplyPropertyValue(Inline.TextDecorationsProperty, null);
-                }
-            }
-
-            //m_mainInput.Focus();
+            this.SetDecorations(new TextRange(Selection.Start, Selection.End),
+                TextDecorations.Underline);
+            return;
+            HandleText(Inline.TextDecorationsProperty, TextDecorations.Underline, null);
         }
 
         private void StrikeThroughBtn_Click(object sender, RoutedEventArgs e)
         {
-            TextSelection selection = m_mainInput.Selection;
-            if (!selection.IsEmpty)
-            {
-                var tdc = (TextDecorationCollection) selection.GetPropertyValue(Inline.TextDecorationsProperty);
-                if (tdc == null || !tdc.SequenceEqual(TextDecorations.Strikethrough))
-                {
-                    selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Strikethrough);
-                }
-                else
-                {
-                    selection.ApplyPropertyValue(Inline.TextDecorationsProperty, null);
-                }
-            }
+            //HandleText(Inline.TextDecorationsProperty, TextDecorations.Strikethrough, null);
+            this.SetDecorations(new TextRange(Selection.Start, Selection.End),
+                TextDecorations.Strikethrough);
+        }
 
-            //m_mainInput.Focus();
+        private void SetDecorations(TextRange textRange, TextDecorationCollection decoration)
+        {
+            var decorations = textRange.GetPropertyValue(Inline.TextDecorationsProperty) as TextDecorationCollection
+                              ?? new TextDecorationCollection();
+
+            decorations = decorations.Contains(decoration.First())
+                ? new TextDecorationCollection(decorations.Except(decoration))
+                : new TextDecorationCollection(decorations.Union(decoration));
+
+            textRange.ApplyPropertyValue(Inline.TextDecorationsProperty, decorations);
         }
     }
 }
